ab68d7b7ec7ce38f6fcbe367d25501db6c5c0a2f
FernandoUc <fernandopoot247@gmail.com>
2025-04-18 15:46:56 -0600

    modificacion de conexion

--- conexion.php
+++ conexion.php
@@ -1,21 +1,18 @@
 <?php
 
-    class BD{
+class BD
+{
 
-        private static $instancia=NULL;
+    private static $instancia = NULL;
 
-        public static function crearInstancia(){
-            if(!isset(self::$instancia)){
-                
-                $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    public static function crearInstancia()
+    {
+        if (!isset(self::$instancia)) {
 
-                self::$instancia = new PDO('mysql:host=localhost;dbname=cfe','root','1234',$opcionesPDO);
-    
-            }
-            return self::$instancia;
-            
+            $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+
+            self::$instancia = new PDO('mysql:host=localhost;dbname=cfe', 'root', '1234', $opcionesPDO);
         }
-
+        return self::$instancia;
     }
-
-?>
+}
